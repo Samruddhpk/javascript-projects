f649b779a773a415aaed188f805d24d7d95ebe92
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-08-20 12:18:00 +0530

    Dark Mode

---
+++ 3_ES6_PROJECTS/2_DARK_MODE/app.js
@@ -0,0 +1,27 @@
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+
+// dark theme
+toggleBtn.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark-theme");
+});
+
+
+// render data
+
+const articlesData = articles.map((article) => {
+    const { title, date, length, snippet } = article;
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+    return `<article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${formatDate}</span>
+            <span>${length} min(s) read</span>
+          </div>
+          <p>
+            ${snippet}
+          </p>
+        </article>`;
+}).join("");
+
+articlesContainer.innerHTML = articlesData;
